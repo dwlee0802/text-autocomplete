459c59de7a9e47019ce9fc9a43b2e551eac348d5
c-degni <christivanndeg@gmail.com>
2025-06-29 16:57:32 -0400

    Fix dropdown not closing on plugin disable (windows), add cs subdict

--- src/dictionary/dictionary.ts
+++ src/dictionary/dictionary.ts
@@ -1,7 +1,10 @@
 import { Trie } from './trie';
-import { DEFAULT_WORDS } from './words'
+import { DEFAULT_WORDS, CS_WORDS } from './words'
 
 let DEFAULT_TRIE = new Trie();
 DEFAULT_WORDS.forEach(word => DEFAULT_TRIE.insert(word));
+
+// May later turn the following dictionaries as separate user-enabled subdictionaries
+CS_WORDS.forEach(word => DEFAULT_TRIE.insert(word));
 
 export { DEFAULT_TRIE };
